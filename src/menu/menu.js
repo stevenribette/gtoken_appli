501f26e1f18dd9171a610c843e0c2b9b12645200
stevenribette <steven.ribette@epsi.fr>
2018-04-11 11:56:41 +0100

    bootstrap is back

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -1,5 +1,15 @@
 class menuComponent{
     template(){
-        $("#menu").load("src/menu/menu.html")
+        $("#menu").load("src/menu/menu.html");
+    }
+    open() {
+        document.getElementById("menu").style.width = "20%";
+        document.getElementById("centralApp").style.marginLeft = "20%";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
+    close() {
+        document.getElementById("menu").style.width = "0";
+        document.getElementById("centralApp").style.marginLeft = "0";
+        document.body.style.backgroundColor = "white";
     }
 }
